c684cf48a1e24462dc202d34c673d638cf67fcd8
Emir Küçükosman <emirkucukosman@Emirs-MBP.home>
2022-09-30 18:29:44 +0300

    feat: error and loading states for products

---
+++ src/components/Products/Loading.tsx
@@ -0,0 +1,24 @@
+export const Loading = () => {
+  return (
+    <div className="bg-white p-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-y-5 gap-x-6">
+        {Array.from({ length: 16 }).map((_, i) => (
+          <div className="animate-pulse">
+            <div className="flex flex-col justify-between gap-2">
+              <div className="flex flex-col gap-1">
+                <div className="border-2 border-[#F3F0FE] p-4 rounded-[12px]">
+                  <div className="bg-[#C4C4C4] h-24"></div>
+                </div>
+                <span className="bg-primary h-3 w-8 rounded-md"></span>
+                <span className="bg-slate-200 h-3 w-16 rounded-md"></span>
+              </div>
+              <button
+                className={`bg-primary w-full h-2 text-white text-sm text-center rounded-sm p-0.5`}
+              ></button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -6,23 +6,36 @@ import { useAppSelector } from "~/app/hook";
 import { selectFilter } from "~/slices/filterSlice";
 
 // Components
-import { ProductCard } from "./ProductCard";
 import { Pagination } from "~/components/Pagination";
+import { ProductCard } from "./ProductCard";
 import { ItemTypes } from "./ItemTypes";
+import { Loading } from "./Loading";
 
 export const Products = () => {
   const filter = useAppSelector(selectFilter);
 
   // Queries
-  const { data } = useGetProductsQuery(filter);
+  const { data, isLoading, isError } = useGetProductsQuery(filter);
 
   return (
     <div className="flex flex-col gap-4">
       <h3 className="text-[#6F6F6F] text-xl">Products</h3>
+      {/* Item Types */}
       <ItemTypes />
+
+      {/* Loading */}
+      {isLoading && <Loading />}
+
+      {/* Error */}
+      {isError && <div className="text-red-500">Something went wrong</div>}
+
+      {/* Data */}
       {data && (
         <div className="bg-white p-4">
+          {/* No products found */}
           {data.products.length === 0 && <span>No products found</span>}
+
+          {/* Products Grid */}
           {data.products.length > 0 && (
             <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-y-5 gap-x-6">
               {data.products.map((product, i) => (
